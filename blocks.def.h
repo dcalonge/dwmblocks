8b1f90307f6de72b9289a74f14510e1848a47a2e
Daniel <dcalonge01@gmail.com>
2026-04-01 22:57:00 +0200

    fix paddings

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,12 @@
 static const Block blocks[] = {
 //   Icon    Command                          Update Interval     Update Signal
-    { "", "/home/daniel/mis_repos/someblocks/blocks/checkup",                 3660,               30 },
-    { "",    "/home/daniel/mis_repos/someblocks/blocks/mic",                               0,                15 },
-    { "",    "/home/daniel/mis_repos/someblocks/blocks/volume",                               0,                10 },
-    { "   ",    "/home/daniel/mis_repos/someblocks/blocks/brightness",                           0,                20 },
-    { "",    "/home/daniel/mis_repos/someblocks/blocks/bluetooth",                               0,                5 },
-    { "   ",    "/home/daniel/mis_repos/someblocks/blocks/wifi",                              2,               0 },
-    { "   ",    "/home/daniel/mis_repos/someblocks/blocks/battery",                              2,               0 },
+    { "", "/home/daniel/mis_repos/dwmblocks/blocks/checkup",                 3660,               30 },
+    { "",    "/home/daniel/mis_repos/dwmblocks/blocks/mic",                               0,                15 },
+    { "",    "/home/daniel/mis_repos/dwmblocks/blocks/volume",                               0,                10 },
+    { "   ",    "/home/daniel/mis_repos/dwmblocks/blocks/brightness",                           0,                20 },
+    { "",    "/home/daniel/mis_repos/dwmblocks/blocks/bluetooth",                               0,                5 },
+    { "   ",    "/home/daniel/mis_repos/dwmblocks/blocks/wifi",                              2,               0 },
+    { "   ",    "/home/daniel/mis_repos/dwmblocks/blocks/battery",                              2,               0 },
     { "   ",    "date '+%H:%M %d/%m '",        30,                0 },
 };
 
